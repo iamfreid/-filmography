26f8780f5a2f59bf25274ffe79a8cd549ebb66b2
IShpak <84473401+Iv4nShpak@users.noreply.github.com>
2023-01-02 23:37:44 +0300

    add new controller
    Добавил crud контроллер и класс MovieCatalog

---
+++ Controllers/CrudController.cs
@@ -0,0 +1,60 @@
+﻿using Filmography.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Filmography.Controllers
+{
+    [Route("api/crud")] //clear read update delete
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        //private readonly MovieCatalog _items = new MovieCatalog();
+
+        private MovieCatalog _item;
+
+        public CrudController(MovieCatalog item)
+        {
+            _item = item;
+        }
+
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(_item.Get());
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery]string input) // "input" - берем из строки запроса. 
+        {
+            _item.Add(input);
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string stringToUpdate, [FromQuery] string newItem)
+        {
+            for (int i = 0; i < _item.Items.Count; i++)
+            {
+                if (_item.Items[i] == stringToUpdate)
+                {
+                    _item.Items[i] = newItem;
+                }
+            }
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringToDelete)
+        {
+            _item.Items = _item.Items.Where(w => w != stringToDelete).ToList();
+            return Ok();
+        }
+
+
+
+
+
+    }
+}
---
+++ Models/MovieCatalog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Filmography.Models
+{
+    public class MovieCatalog
+    {
+        private List<string> _values;
+
+        public MovieCatalog() 
+        {
+            _values = new List<string>();
+        }
+
+        public void Add(string value)
+        {
+            _values.Add(value);
+        }
+
+        public string Get() 
+        {
+            return string.Join(",", _values.ToArray());
+        }
+
+        public List<string> Items 
+        {
+            get { return _values; }
+            set { _values = value; }
+        }
+    }
+}
